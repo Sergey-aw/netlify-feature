86fb91ba111c5ed424ae311a2997265dd97d463a
Sergey-aw <sergey@berisamocat.ru>
2023-12-08 21:45:41 +0300

    Update components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,25 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
   integrations: [preact(), sitemap({
     canonicalURL: 'https://feature-tour.netlify.app'
-  }), tailwind()]
+  }), tailwind(), 
+
+  storyblok({
+    accessToken: "0pVyrxo9wIEd5K2FFPLA1gtt-222699-iaxjtUnzWM_mxb3uzyU_",
+    components: {
+      page: "storyblok/Page",
+      feature: "storyblok/Feature",
+      grid: "storyblok/Grid",
+      teaser: "storyblok/Teaser",
+    },  
+  })]
 });
+
+
