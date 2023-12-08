5aedc86c55dfcd1ed49218c3bf073080e0fced64
Sergey-aw <sergey@berisamocat.ru>
2023-12-08 21:50:22 +0300

    Update dependecies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import storyblok from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,15 +11,7 @@ export default defineConfig({
     canonicalURL: 'https://feature-tour.netlify.app'
   }), tailwind(), 
 
-  storyblok({
-    accessToken: "0pVyrxo9wIEd5K2FFPLA1gtt-222699-iaxjtUnzWM_mxb3uzyU_",
-    components: {
-      page: "storyblok/Page",
-      feature: "storyblok/Feature",
-      grid: "storyblok/Grid",
-      teaser: "storyblok/Teaser",
-    },  
-  })]
+ 
 });
 
 
