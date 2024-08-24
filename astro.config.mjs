57f9c1ddd02fe7c5f8e2f513a3db027e67f6d4ea
Sergey-aw <sergey@berisamocat.ru>
2024-08-24 22:37:24 +0300

    added photo and change main page layout into 2 columns using div

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,27 @@ import preact from "@astrojs/preact";
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
-  integrations: [ preact(),
-                  react(),
+  integrations: [ preact({
+    include: ['**/preact/*'],
+  }),
+                  react({
+                    include: ['**/react/*'],
+                  }),
                   sitemap({
     canonicalURL: 'https://feature-tour.netlify.app'
   }), tailwind(), 
-
- ]
-});
+                ],
+  i18n: {
+                  defaultLocale: "ru",
+                  locales: ["en","ru"],
+  },
+}
+);
 
 
