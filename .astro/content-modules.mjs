d07970694dde550e8d5ed70d3501c0c8ea9c5bc7
Sergey-aw <sergey@berisamocat.ru>
2025-03-15 23:35:14 +0300

    fix build

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/blog/mdx-example.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Fmdx-example.mdx&astroContentModuleFlag=true")],
-["src/blog/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Ftest.mdx&astroContentModuleFlag=true")]]);
+["src/blog/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Ftest.mdx&astroContentModuleFlag=true")],
+["src/blog/mdx-example.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Fmdx-example.mdx&astroContentModuleFlag=true")]]);
 		
