6ad6b9cea25ba94bcff9ba6a071595cef8d0658c
Sergey-aw <sergey@berisamocat.ru>
2025-05-01 00:31:55 +0300

    updated blog layout

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,7 +2,10 @@
 export default new Map([
 ["src/blog/post2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Fpost2.mdx&astroContentModuleFlag=true")],
 ["src/blog/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Ftest.mdx&astroContentModuleFlag=true")],
-["src/blog/mdx-example.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Fmdx-example.mdx&astroContentModuleFlag=true")],
 ["src/blog-en/post2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog-en%2Fpost2.mdx&astroContentModuleFlag=true")],
-["src/blog-en/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog-en%2Ftest.mdx&astroContentModuleFlag=true")]]);
+["src/blog/mdx-example.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Fmdx-example.mdx&astroContentModuleFlag=true")],
+["src/blog-en/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog-en%2Ftest.mdx&astroContentModuleFlag=true")],
+["src/blog-en/post2 copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog-en%2Fpost2+copy.mdx&astroContentModuleFlag=true")],
+["src/blog-en/post1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog-en%2Fpost1.mdx&astroContentModuleFlag=true")],
+["src/blog/post1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblog%2Fpost1.mdx&astroContentModuleFlag=true")]]);
 		
